eac226d532f37108f19508242a1a4d1461a5f2b0
Josue Rivera <josuerive20052323@gmail.com>
2025-06-02 00:08:49 -0600

    Terminado

--- src/components/molecules/MovieCardTop10.tsx
+++ src/components/molecules/MovieCardTop10.tsx
@@ -11,16 +11,17 @@ interface MovieCardTop10Props {
 const MovieCardTop10: React.FC<MovieCardTop10Props> = ({ movie, index }) => (
   <div className="d-flex align-items-end position-relative px-2" style={{ minHeight: '180px' }}>
     <h1
-      className="fw-bold text-white"
-      style={{
-        fontSize: '4.5rem',
-        opacity: 0.15,
-        marginRight: '15px',
-        userSelect: 'none',
-        textShadow: '2px 2px 10px rgba(0,0,0,0.8)',
-        minWidth: '60px',
-        lineHeight: 1,
-      }}
+       className="hbo-number"
+  style={{
+    fontSize: '6rem',
+    fontWeight: 900,
+    color: '#555', // gris oscuro
+    marginRight: '15px',
+    opacity: 1,
+    textShadow: '3px 3px 10px rgba(0, 0, 0, 0.9)', // sombra negra fuerte
+    lineHeight: 1,
+    userSelect: 'none',
+  }}
     >
       {index + 1}
     </h1>
